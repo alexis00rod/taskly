31e33f105457789e9c116372361bb77c55437baa
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-06-10 20:42:03 -0300

    clean code

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { App } from "@components";
 import "@styles";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}></Route>
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
