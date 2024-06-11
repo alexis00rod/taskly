6e049be7f5804efa2603e5c8fee177ed0129e6e5
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-06-10 21:33:21 -0300

    add: auth page styles

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -1,12 +1,21 @@
 import { NavLink, Outlet } from "react-router-dom";
+import AuthImage from "../../assets/images/auth-image.png";
 
 const AuthPage = () => {
   return (
-    <div>
-      <h2>Taskly</h2>
-      <NavLink to="/auth/login">Iniciar sesion</NavLink>
-      <NavLink to="/auth/signup">Registrarse</NavLink>
-      <Outlet />
+    <div className="authPage">
+      <figure className="authPage-left">
+        <img src={AuthImage} alt="Auth Image" />
+      </figure>
+      <div className="authPage-right">
+        <div className="authPage-header">
+          <h2>Taskly</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <NavLink to="/auth/login">Iniciar sesion</NavLink>
+        <NavLink to="/auth/signup">Registrarse</NavLink>
+        <Outlet />
+      </div>
     </div>
   );
 };
