666ea837ef7d1f1d6718779d91070510d2ec88a5
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-06-10 21:31:55 -0300

    add: font poppins and colors custom blue, purple and pink

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,37 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        // Extralight: 60%
+        // Light: 20%
+        // Main: 0%
+        // Dark: 20%
+        // Blue
+        customBlueExtraLight: "#b8ebed",
+        customBlueLight: "#71d6dc",
+        customBlue: "#4DCCD3",
+        customBlueDark: "#3ea3a9",
+        // Purple
+        customPurpleExtraLight: "#cdcafc",
+        customPurpleLight: "#9c95f9",
+        customPurple: "#837af8",
+        customPurpleDark: "#6962c6",
+        // Pink
+        customPinkExtraLight: "#ffe1e1",
+        customPinkLight: "#ffc2c2",
+        customPink: "#ffb3b3",
+        customPinkDark: "#cc8f8f",
+        // Theme
+        customLightTheme: "#fcfcf5",
+        customDarkTheme: "#494942",
+        customWhite: "#FEFCFB",
+        customBlack: "#101419",
+      },
+    },
   },
   plugins: [],
 };
