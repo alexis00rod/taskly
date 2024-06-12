250afc45597da63ae7bc745a8095e3b36ef00442
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-06-11 21:19:20 -0300

    add: button and icon components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export { default as App } from "./App/App";
 export { default as Textfield } from "./Textfield/Textfield";
+export { default as Button } from "./Button/Button";
+export { default as Icon } from "./Icon/Icon";
