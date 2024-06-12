b1d47acc2a26ef0a7de5db27c7a01a7a4e79d017
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-06-11 21:23:16 -0300

    fix: auth page styles

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -1,19 +1,17 @@
-import { NavLink, Outlet } from "react-router-dom";
-import AuthImage from "../../assets/images/auth-image.png";
+import { Outlet } from "react-router-dom";
+import AuthImage from "@assets/images/auth-image.png";
 
 const AuthPage = () => {
   return (
     <div className="authPage">
-      <figure className="authPage-left">
+      <figure className="authPage-image">
         <img src={AuthImage} alt="Auth Image" />
       </figure>
-      <div className="authPage-right">
+      <div className="authPage-container">
         <div className="authPage-header">
           <h2>Taskly</h2>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         </div>
-        <NavLink to="/auth/login">Iniciar sesion</NavLink>
-        <NavLink to="/auth/signup">Registrarse</NavLink>
         <Outlet />
       </div>
     </div>
