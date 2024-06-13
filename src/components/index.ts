ca9a1fabd780175fc3cdab73ff760f3919b4cbba
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-06-12 23:48:40 -0300

    add: dashboard component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as App } from "./App/App";
 export { default as Textfield } from "./Textfield/Textfield";
 export { default as Button } from "./Button/Button";
 export { default as Icon } from "./Icon/Icon";
+export { default as Dashboard } from "./Dashboard/Dashboard";
