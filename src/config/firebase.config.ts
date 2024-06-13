2aac50e5847511944b9216e006a15f9df7229757
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-06-13 18:45:55 -0300

    add: firebase config

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,15 @@
+import { FirebaseOptions, initializeApp } from "firebase/app";
+import * as auth from "firebase/auth";
+
+const firebaseConfig: FirebaseOptions = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+};
+
+export const appFirebase = initializeApp(firebaseConfig);
+export const authFirebase = auth.getAuth(appFirebase);
