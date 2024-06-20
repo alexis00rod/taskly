21c0069caa97fa563c21aabd5f3556acd9234709
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-06-19 23:28:24 -0300

    add: getProjectsDoc function

--- src/services/projects.ts
+++ src/services/projects.ts
@@ -1,8 +1,10 @@
 import { dbFirebase } from "config/firebase.config";
 import * as db from "firebase/firestore";
+import { ProjectTypes } from "@models";
 // References
 const projectsRef = () => db.collection(dbFirebase, "projects");
 
+// Function to create project doc
 export const createProjectDoc = async (name: string, uid: string) => {
   const projectId = await db.addDoc(projectsRef(), {
     name,
@@ -11,4 +13,22 @@ export const createProjectDoc = async (name: string, uid: string) => {
     updateAt: db.serverTimestamp(),
   });
   return projectId.id;
+};
+
+// Function to get project doc
+export const getProjectsDoc = (
+  uid: string,
+  callback: (projects: ProjectTypes[]) => void
+) => {
+  const projectQuery = db.query(projectsRef(), db.where("ownerId", "==", uid));
+
+  const unsubscribe = db.onSnapshot(projectQuery, (snapshot) => {
+    const projects = snapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    })) as ProjectTypes[];
+    callback(projects);
+  });
+
+  return unsubscribe;
 };
