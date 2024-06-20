af01606a84a4b2d71a7b05cbd2769140eabfb840
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-06-19 22:03:21 -0300

    add: firstProject route

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,5 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import { AuthRoute, DashboardRoute } from "@routes";
+import { AuthRoute, DashboardRoute, FirstProjectRoute } from "@routes";
 import { useAuth } from "@hooks";
 
 const App: React.FC = () => {
@@ -11,6 +11,7 @@ const App: React.FC = () => {
         <Route path="/" element={<Navigate to="/dashboard" />} />
         <Route path="/dashboard/*" element={<DashboardRoute />} />
         <Route path="/auth/*" element={<AuthRoute />} />
+        <Route path="/first-project/*" element={<FirstProjectRoute />} />
       </Routes>
     </div>
   );
