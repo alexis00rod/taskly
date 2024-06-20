4d33e24591e599e542028ff31a12ea4a20d73c14
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-06-19 22:06:13 -0300

    add: createTaskDoc function

---
+++ src/services/tasks.ts
@@ -0,0 +1,21 @@
+import { dbFirebase } from "config/firebase.config";
+import * as db from "firebase/firestore";
+
+// References
+const tasksRef = (project: string) =>
+  db.collection(dbFirebase, "projects", project, "tasks");
+
+export const createTaskDoc = async (
+  name: string,
+  project: string,
+  assignedTo: string
+) => {
+  return await db.addDoc(tasksRef(project), {
+    name,
+    assignedTo,
+    createdAt: db.serverTimestamp(),
+    updateAt: db.serverTimestamp(),
+    status: "pending",
+    priority: "low",
+  });
+};
