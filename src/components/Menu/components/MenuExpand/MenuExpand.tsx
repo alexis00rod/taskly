80dba14db1cfa044279b16c66c5622d35fdea782
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-06-24 20:06:42 -0300

    add: menu-position prop

--- src/components/Menu/components/MenuExpand/MenuExpand.tsx
+++ src/components/Menu/components/MenuExpand/MenuExpand.tsx
@@ -4,13 +4,22 @@ import { useMenuContext } from "../../MenuContext";
 interface MenuExpandProps {
   children: ReactNode;
   divide?: boolean;
+  position?: string;
 }
 
-const MenuExpand: React.FC<MenuExpandProps> = ({ children, divide }) => {
+const MenuExpand: React.FC<MenuExpandProps> = ({
+  children,
+  divide,
+  position = "right",
+}) => {
   const { isOpen, expand } = useMenuContext();
 
   return (
-    <div className={`menu-container ${isOpen ? "flex" : "hidden"}`}>
+    <div
+      className={`menu-container ${
+        isOpen ? "flex" : "hidden"
+      } menu-${position}`}
+    >
       <div
         className={`menu-container-expand ${expand ? "open" : ""} ${
           divide ? "divide-y divide-customDarkTheme" : ""
